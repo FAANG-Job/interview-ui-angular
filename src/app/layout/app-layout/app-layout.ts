d4ef9494702315089b78f13afe1e070137cbceb4
rohit-ai-architect <61180510+rohit-ai-architect@users.noreply.github.com>
2026-09-24 18:16:29 +0530

    Add support for service and model for Interviews.

--- src/app/layout/app-layout/app-layout.ts
+++ src/app/layout/app-layout/app-layout.ts
@@ -1,56 +1,21 @@
 import { Component } from '@angular/core';
-export type InterviewStatus = 'Scheduled' | 'Completed' | 'Cancelled';
 import { Router, RouterOutlet } from '@angular/router';
-
-export interface Interview {
-  id: number;
-  candidateName: string;
-  status: InterviewStatus;
-  scheduledAt: Date;
-  role: string;
-  interviewer: String;
-}
-
+import { InterviewStatus, Interview } from '../../models/interview';
+import { InterviewService } from '../../services/interview.service';
 
 @Component({
   selector: 'app-app-layout',
-  imports: [RouterOutlet ],
+  imports: [RouterOutlet],
   templateUrl: './app-layout.html',
   styleUrl: './app-layout.scss',
 })
 
 
 export class AppLayout {
-  constructor(private router: Router) {}
-
-
-  interviews: Interview[] = [
-    {
-      id: 100,
-      candidateName: "Rohit",
-      status: "Completed",
-      scheduledAt: new Date('2026-09-24T10:00:00'),
-      role: "Java Backend Developer",
-      interviewer: "Rohit Aggarwal"
-    },
-    {
-      id: 200,
-      candidateName: "Mohit",
-      status: "Cancelled",
-      scheduledAt: new Date('2026-09-24T10:00:00'),
-      role: "FE Developer",
-      interviewer: "Garv Aggarwal"
-    },
-    {
-      id: 300,
-      candidateName: "Rajesh",
-      status: "Scheduled",
-      scheduledAt: new Date('2026-09-24T10:00:00'),
-      role: "FE Developer",
-      interviewer: "Garv Aggarwal"
-    }
-  ];
-
+  private interviews: Interview[] = [];
+  constructor(private router: Router, private interviewService: InterviewService) {
+    this.interviews = interviewService.getInterviews();
+  }
   get completedCount(): number {
     return this.interviews.filter(i => i.status === 'Completed').length;
   }
